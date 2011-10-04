74e7af37826865b0259631f235cceeb8529d14f6
Jeff Barczewski <jeff.barczewski@gmail.com>
2011-10-04 13:50:15 -0500

    added folder support for articles

---
+++ example2/views/home.js
@@ -0,0 +1,50 @@
+
+require.paths.push('/slotsghp-blog/petrify/lib');
+var html_escape = require("petrify").html_escape;
+
+// wait until the navigation and articles views are complete
+exports.requires = ['navigation', 'articles'];
+
+//function html_escape(s) { 
+//    return s.split('&').join('&amp;').split( '<').join('&lt;').split('>').join('&gt;').split('"').join('&quot;'); 
+//	}
+
+exports.run = function(view, context){
+
+    var sorted_articles = context.data.sort(function(a, b) {
+          return a.heading.localeCompare(b.heading);
+	});
+
+    // render the home template
+    var html = context.templates['home.jsont'].expand({
+	    articles: sorted_articles,
+	    partials: context.partials
+    });
+
+    html = html.replace( /__BASE_PREFIX__/g , ''); //replace prefix in nav and other partials
+
+
+    // prepare xml for feed by rendering each article in xml
+    var articles_xml = [];
+    sorted_articles.forEach(function(value,index,array) {
+	    value.escaped_html_no_heading = html_escape(value.html_no_heading);
+
+	    var article_xml = context.templates['article_xml.jsont'].expand({
+		    article: value,
+		    partials: {}
+		});
+	    articles_xml.push(article_xml);
+	});
+
+    // render the xml feed
+    var feed_xml = context.templates['home_xml.jsont'].expand({
+	    articles_xml:articles_xml,
+	    updated:"test",
+	    partials: {}
+	});
+
+    view.emit('index.html', html);
+    view.emit('atom.xml', feed_xml);
+
+    view.done();
+};
---
+++ example2/views/years.js
@@ -0,0 +1,28 @@
+// wait until the navigation and articles views are complete
+exports.requires = ['navigation', 'articles'];
+
+
+exports.run = function(view, context){
+
+    // use the list of unique years generated in the naviagtion view
+    context.partials.years.forEach(function(year){
+
+        // get the articles in that year
+        var articles = context.data.filter(function(d){
+            return d.date.getFullYear() == year;
+        });
+
+        // render the year template
+        var html = context.templates['year.jsont'].expand({
+            name: year,
+            articles: articles,
+            partials: context.partials
+        });
+
+        html = html.replace( /__BASE_PREFIX__/g , ''); //replace prefix in nav and other partials
+        view.emit(year + '.html', html);
+
+    });
+
+    view.done();
+};
